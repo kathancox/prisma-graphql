a305bd3e4f2f22b56a8ccc1d78ae8365b15916c1
kathancox <khancox@digitalocean.com>
2020-10-22 09:01:01 -0400

    Add Prisma

--- src/schema.js
+++ src/schema.js
@@ -1,3 +1,4 @@
+const { prisma } = require('./db')
 const { gql } = require('apollo-server')
 
 const typeDefs = gql`
@@ -19,57 +20,38 @@ const typeDefs = gql`
   }
 `
 
-const posts = [
-  {
-    id: 1,
-    title: 'Subscribe to GraphQL Weekly for community news ',
-    content: 'https://graphqlweekly.com/',
-    published: true,
-  },
-  {
-    id: 2,
-    title: 'Follow DigitalOcean on Twitter',
-    content: 'https://twitter.com/digitalocean',
-    published: true,
-  },
-  {
-    id: 3,
-    title: 'What is GraphQL?',
-    content: 'GraphQL is a query language for APIs',
-    published: false,
-  },
-]
-
 const resolvers = {
   Query: {
     feed: (parent, args) => {
-      return posts.filter((post) => post.published)
+      return prisma.post.findMany({
+        where: { published: true },
+      })
     },
     post: (parent, args) => {
-      return posts.find((post) => post.id === Number(args.id))
+      return prisma.post.findOne({
+        where: { id: Number(args.id) },
+      })
     },
   },
   Mutation: {
     createDraft: (parent, args) => {
-      posts.push({
-        id: posts.length + 1,
-        title: args.title,
-        content: args.content,
-        published: false,
+      return prisma.post.create({
+        data: {
+          title: args.title,
+          content: args.content,
+        },
       })
-      return posts[posts.length - 1]
     },
     publish: (parent, args) => {
-      const postToPublish = posts.find((post) => post.id === Number(args.id))
-      postToPublish.published = true
-      return postToPublish
+      return prisma.post.update({
+        where: {
+          id: Number(args.id),
+        },
+        data: {
+          published: true,
+        },
+      })
     },
-  },
-  Post: {
-    content: (parent) => parent.content,
-    id: (parent) => parent.id,
-    published: (parent) => parent.published,
-    title: (parent) => parent.title,
   },
 }
 
